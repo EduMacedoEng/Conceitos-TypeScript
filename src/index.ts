698660b2649ae0553c5ce5605bd6b70b0c85580b
Eduardo Macedo <eduardomacedo.eng@hotmail.com>
2020-09-30 10:36:36 -0300

    Config Project

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+
+app.listen(3333);
+
+app.get('/', (request, response) => {
+    return response.json({message: 'Hello World'});
+})
+
+app.listen(3333);
